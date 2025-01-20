cfc7d85b2b5cfbd03ecbb1ea86413ab8c643f03f
jurijreact <f888@gmail.com>
2025-01-20 14:47:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import React, { Fragment } from "react";
+import "./App.css";
+
+import { useEffect, useState } from "react";
+
+function App() {
+
+  const [state, setState] = useState(
+      // can read Cart from server {cartID:'wTOPiuPEKGUm5qvyzJKv0OKfUUomCYcD'}
+  )
+
+  return(
+    <div>
+        <div>App</div>
+
+    </div>
+  );
+}
+
+export default App;
